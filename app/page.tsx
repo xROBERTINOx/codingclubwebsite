0b6fa6f74171b04c2c91756519236b79b273f11c
roberto <robert.barkalov@gmail.com>
2024-08-29 23:13:14 -0400

    shaped

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,7 @@
-'use client';
-import React, { useState } from 'react';
+import React from 'react';
 import Link from 'next/link';
 
 export default function Home() {
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [apiResponse, setApiResponse] = useState<string | null>(null);
-
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    try {
-      const response = await fetch('/api/data', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ firstName, lastName }),
-      });
-      const data = await response.json();
-      setApiResponse(data.message);
-    } catch (error) {
-      console.error('Error submitting form:', error);
-      setApiResponse('Error occurred while processing your submission');
-    }
-  };
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
@@ -36,6 +13,9 @@ export default function Home() {
       <div className="relative flex place-items-center">
         <h1 className="text-4xl font-bold">Coding Club Homepage</h1>
       </div>
+
+      <br></br>
+      <br></br>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-2 lg:text-left">
         <div className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
@@ -59,16 +39,6 @@ export default function Home() {
 
       <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScKxOp9Dj8PEeToa0PwdRlLCyqPb9uQdC9QAG2pkKFGijCjfg/viewform?embedded=true" width="640" height="551">Loading…</iframe>
 
-      <div className="mt-8">
-        <Link href="/signup" className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
-          <span className="text-2xl font-semibold">
-            Sign Up Now{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </span>
-        </Link>
-      </div>
     </main>
   );
 }
